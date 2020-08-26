051b2d8b772641e26662d59c1d1a75572e18f0b8
Nav Dhamu <n4vdeep@gmail.com>
2020-08-26 22:17:11 +0100

    set up basic state for Tourlist and applied logic to map through tourData.js file and display information

--- city-tours/src/components/TourList/index.js
+++ city-tours/src/components/TourList/index.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import Tour from '../Tour/Tour';
+import Tour from '../Tour';
+import './tourlist.scss';
+import { tourData } from '../../tourData';
+
 
 class TourList extends Component {
+  /* 
+    Set up state...
+    This state object references the 
+    tourdata.js file being imported
+    above
+  */
+  state = {
+    tours: tourData
+  };
+
+  removeTour = id => {
+    console.log(id);
+  }
+
   render() {
+    //console.log(this.state.tours);
+    const { tours } = this.state
+
     return (
-      <div>
-        The Tour List
-        <Tour />
-      </div>
+      <section className="tour__list">
+        {/*
+          Loop through tourData...
+          each and every time we get an item
+          return the information from the item
+        */}
+
+        {tours.map(tour => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
     );
   }
 }
