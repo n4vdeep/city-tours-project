2d2503ae2e6cded6d0d65e356ed191533f0125f4
Nav Dhamu <n4vdeep@gmail.com>
2020-08-26 19:11:53 +0100

    initial set up of SASS google fonts and font awesome links

---
+++ city-tours/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  return (
+    <div>
+      <h1>The Current App</h1>
+      <i className="fas fa-home fa-7x" />
+    </div>
+  );
+}
+
+export default App;
